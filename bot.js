7537f975ac35218d90c10a05f5fbba3b9030f599
NaarMc <57398234+NaarMc@users.noreply.github.com>
2019-11-13 22:31:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,6 +164,131 @@ client.on('message', msg => {
     msg.reply('بلغ مسؤول التشهير!');
   }
 });
+const client2 = new Discord.Client();
+const myid = ['639105286266683392'];
+const prefix = ['$'];
+ 
+client.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : Lion `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
+});
+ 
+client2.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+ 
+ 
+ 
+client.on('message', message => {
+    if(message.content === prefix+'راتب'){
+        message.channel.send('#daily')
+    }
+});
+ 
+client.on('message', message => {
+    if(message.content === prefix+'مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+ 
+client.on('message', message => {
+    if(message.content === prefix+'ريب'){
+        message.channel.send("#rep "+"<@" + myid + ">")
+    }
+});
+ 
+client.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**NEW spam code 2019 by Lion  - الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر ر الله اكبر الله اكبر **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+         
+        }
+      }
+});
+ 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+ 
+ 
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('name', 'Role.Lion'); //لازم تعمل الرتبه دي في السيرفر  Role.Lion
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
+ 
+client2.on('message', message => {
+    if(message.content === prefix+'راتب'){
+        message.channel.send('#daily')
+    }
+});
+ 
+client2.on('message', message => {
+    if(message.content === prefix+'مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+ 
+client2.on('message', message => {
+    if(message.content === prefix+'ريب'){
+        message.channel.send("#rep "+"<@" + myid + ">")
+    }
+});
+ 
+client2.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**NEW spam code 2019 by Lion - الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر ر الله اكبر الله اكبر **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+         
+        }
+      }
+});
+ 
+client2.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+ 
+ 
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('name', 'Role.Lion');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
 
 
 client.login(process.env.BOT_TOKEN);
