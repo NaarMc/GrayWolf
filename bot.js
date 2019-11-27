2fae59144d56d6243dce606320dfe64eafbbd97b
NaarMc <57398234+NaarMc@users.noreply.github.com>
2019-11-27 22:18:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1288,7 +1288,16 @@ return;
 }
 
 });
-
+   client.on('message', omar => {
+var prefix = "-";
+if(omar.content.split(' ')[0] == prefix + 'dc') {  
+if (!omar.channel.guild) return;
+if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
+if(!omar.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**");
+omar.guild.channels.forEach(m => {
+m.delete();
+});
+}
 
 
 
