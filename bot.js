25c79090971db04887a16a0cae556e621d3977d5
NaarMc <57398234+NaarMc@users.noreply.github.com>
2019-11-29 23:15:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2085,5 +2085,49 @@ client.on('message', msg => {
             message.channel.send("تم تغيير الاسم الى: " + changenick + "")
         }
     }});
+client.on('message', message => {
+
+
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id == 639105286266683392) return;
+
+
+if (message.content.startsWith(prefix + 'playing')) {
+if (message.author.id !== '639105286266683392','639105286266683392') return message.reply(' هذا الأمر فقط لصاحب البوت و شكراًً ')
+client.user.setGame(argresult);
+    message.channel.sendMessage(**${argresult}** : تم تغيير الحالة)
+} 
+
+ 
+if (message.content.startsWith(prefix + 'streem')) {
+if (message.author.id !== '639105286266683392','639105286266683392') return message.reply(' هذا الأمر فقط لصاحب البوت و شكراًً ')
+client.user.setGame(argresult, "http://twitch.tv/y04zgamer%22);
+    message.channel.sendMessage(**${argresult}** :تم تغيير الحالة الى ستريمنج)
+} else
+
+if (message.content.startsWith(prefix + 'name')) {
+if (message.author.id !== '639105286266683392','639105286266683392') return message.reply(' هذا الأمر فقط لصاحب البوت و شكراًً ')
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(**${argresult}** : تم تغير الأسم)
+  return message.reply("لا تستطيع تغير الأسم الا بعد ساعتين");
+} else
+
+if (message.content.startsWith(prefix + 'image')) {
+if (message.author.id !== '639105286266683392','639105286266683392') return message.reply(' هذا الأمر فقط لصاحب البوت و شكراًً ')
+client.user.setAvatar(argresult);
+    message.channel.sendMessage(**${argresult}** : تم تغير صورة البوت);
+} else
+
+
+if (message.content.startsWith(prefix + 'watching')) {
+if (message.author.id !== '639105286266683392','639105286266683392') return message.reply(' هذا الأمر فقط لصاحب البوت و شكراًً ')
+    client.user.setActivity(argresult, {type : 'watching'});
+ message.channel.sendMessage(**${argresult}** : تم تغيير الووتشينق الى)
+}
+
+ });
+
 
 client.login(process.env.BOT_TOKEN);
