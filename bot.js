27bd253dd05be16b48930d80f0e5211f98f6ffa8
NaarMc <57398234+NaarMc@users.noreply.github.com>
2019-11-30 17:18:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2108,7 +2108,45 @@ client.channels.get("644821003716919297").send(' ***  BOT  ***   **Join To**   *
   }
 }
  });
-
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("644566852785274880");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+ 
+ 
+ 
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("644641237529526292");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("644566852785274880");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;        
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
 
 
 client.login(process.env.BOT_TOKEN);
