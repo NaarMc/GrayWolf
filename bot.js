98fef23805884d31962b00aa5a0affa1707adc73
NaarMc <57398234+NaarMc@users.noreply.github.com>
2019-11-30 18:24:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2134,34 +2134,13 @@ message.channel.send(`${user} لقد قمت بدعوه ${inviteCount} دعوه.`
 });
   }
 });
-hero.on('message',async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === 'dm') return;
-  let args = message.content.split(' ');
-  let tag;
-  if(args[0] === `${prefix}discrim`) {
-    if(args[1]) {
-      let discrim = Array.from(args[1]);
-      if(isNaN(args[1])) return message.channel.send(`- \`${message.author.username}\`, يجب ان تتكون هذه الخانة من ارقام وليس احرف`);
-      if(discrim.length !== 4) return message.channel.send(`- \`${message.author.username}\`, يجب ان يكون التاق مكون من 4 ارقام`);
-
-      tag = discrim.map(r => r.toString()).join('');
-      console.log(tag);
-      if(hero.users.filter(f => f.discriminator === tag).size === 0) return message.channel.send(`- \`${message.author.username}\`, لا يوجد احد بهذا التاق`);
-      let iLD = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setDescription(hero.users.filter(f => f.discriminator === tag).map(r => r.username).slice(0, 10).join('\n'))
-      .setFooter('By: xYouseeF\'₁₁ || Roýale.#0001');
-      message.channel.send(iLD);
-    } else if(!args[1]) {
-      tag = message.author.discriminator;
-      if(hero.users.filter(f => f.discriminator === tag).size === 0) return message.channel.send(`- \`${message.author.username}\`, لا يوجد احد بهذا التاق`);
-      let L4U = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setDescription(hero.users.filter(f => f.discriminator === tag).map(r => r.username).slice(0, 10).join('\n'))
-      .setFooter('By: xYouseeF\'₁₁ || Roýale.#0001');
-      message.channel.send(L4U);
-    }
+const swearWords = ["كس اختك", "كل زق", "خرية", "زبلة", "كس اختك", "فاك", "فاك يو", "انيكلك", "قحبة", "كس امك", "نيك امك", "قحب"]; 
+  if( swearWords.some(word => message.content.includes(word)) ) {
+    message.reply("مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ").then(sentMessage =>{
+      sentMessage.delete(20000)
+    })
+    message.delete(3000)
+    client.channels.get('644821003716919297').send(message.author.toString() + "استخدم كلام لا يليق ~")
   }
 });
 
