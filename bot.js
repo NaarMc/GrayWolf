20ae56bf619eb08f97e9e1ebecca8bad3b7e4376
NaarMc <57398234+NaarMc@users.noreply.github.com>
2019-12-11 19:59:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2127,7 +2127,7 @@ client.on('message', message => {
     .addField("**🆔 Server ID**", "**"+message.guild.id+"**",true)
     .addField("**👑 Owned by**", "**"+message.guild.owner+"**" ,true)
     .addField("**🌍 Others**" , "**"+message.guild.region+"**",true)
-    .addField('**💬 channels**',`**[ ${message.guild.channels.filter(m => m.type === 'text').size} ]text**`,true)[ ${message.guild.channels.filter(m => m.type === 'voice').size} ] voice** `,true)
+    .addField('**💬 channels**',`**[ ${message.guild.channels.filter(m => m.type === 'text').size} ]text**${msg.guild.channels.filter(m => m.type === 'voice').size}`,true)
     .addField("**🤔 Created On**", ` ** [ ${days.toFixed(0)} ] ** Day ` ,true)
     
     .addField("👥Members",`
