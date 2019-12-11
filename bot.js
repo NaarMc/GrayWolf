0fa4cc988e5198bd564dcb0aa01395e9c8bbe740
NaarMc <57398234+NaarMc@users.noreply.github.com>
2019-12-11 23:20:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1886,32 +1886,7 @@ message.channel.send(roleinfo);
 }
 });
 
-client.on('message', message => {
-    if (message.content === "-server") {
-    if(!message.channel.guild) return;
-    const millis = new Date().getTime() - message.guild.createdAt.getTime();
-    const now = new Date();
-   
-    const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
-    const days = millis / 1000 / 60 / 60 / 24;
-    let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
-    var embed  = new Discord.RichEmbed()
-    .setAuthor(message.guild.name, message.guild.iconURL)
-    .addField("**🆔 Server ID**", ""+message.guild.id+"",true)
-    .addField("**🤔 Created On**", `  [ ${days.toFixed(0)} ]  Day ` ,true)
-    .addField("**👑 Owned by**", "**"+message.guild.owner+"**" ,true)
-    .addField("👥Members",`**${message.guild.memberCount}**`)    
-    .addField('**💬 Channels**',`**[ ${message.guild.channels.filter(m => m.type === 'text').size} ] Text **[ ${message.guild.channels.filter(m => m.type === 'voice').size} ] Voice `,true)
-    .addField("**🌍 Others**" , "**"+message.guild.region+"**",true)
-    
-    .addField("**👔 Roles**",`
-**[${message.guild.roles.size}]** Role `,true)
-    
-    .setColor('RANDOM')
-    message.channel.sendEmbed(embed)
-    
-    }
-    });
+
 
 
 client.login(process.env.BOT_TOKEN);
